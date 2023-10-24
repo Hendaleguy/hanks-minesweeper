2e943063077f0c0db7cba90d7b4cfa93bffab614
Hendaleguy <hendaleguy@gmail.com>
2023-10-24 12:55:49 -0500

    bruh

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{html,js,ts,jsx,tsx,vue}"
-  ],
+export default {
+  content: [],
   theme: {
     extend: {},
   },
